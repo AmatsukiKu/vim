4ac4a1044e62965ddcde76d06abc18cd9b13aaff
AmatsukiKu <amatsukikuu.gmail.com>
2014-12-01 18:40:56 +0900

    初期化スクリプトの追加

---
+++ init.sh
@@ -0,0 +1,7 @@
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
+ln -s "${script_dir}/.vimrc" ~/.vimrc
+ln -s "${script_dir}/.gvimrc" ~/.gvimrc
+ln -s "${script_dir}/.vim" ~/.vim
+ln -s "${script_dir}/.vimperatorrc" ~/.vimperatorrc
+ln -s "${script_dir}/vimperator" ~/.vimperator
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
